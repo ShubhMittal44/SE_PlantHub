e5d0d9b9bc0e4ebd9aa298ac3f780b736e7e5371
Shubh <shubhmittal445@gmail.com>
2022-04-16 22:52:31 +0530

    PlantHub

---
+++ blogs/models.py
@@ -0,0 +1,11 @@
+# from django.db import models
+# from django.conf import settings
+#
+
+
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+# Create your models here.
+
